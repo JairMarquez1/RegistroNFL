448e7de5b566feaab9101cfa809ec7ec28825b94
JairMarquez1 <a19310186@ceti.mx>
2020-09-08 03:24:32 -0500

    Version 1

---
+++ main1.js
@@ -0,0 +1,266 @@
+let res = ['_','_','_','_','_','_','_','_','_','_','_','_','_','_'];
+let quantity = localStorage.getItem('quantityNFL');
+let name = localStorage.getItem("aliasNFL");
+let combinations = false;
+var aux;
+
+function start(){
+    recovername();
+    display = document.getElementById("display");
+    let width = document.getElementsByClassName('quiniela')[0].offsetWidth;
+    document.getElementById("quiniela").style.height = width*0.9375 + "px";
+    let container = document.getElementById("text");
+    container.innerHTML = res.join("\xa0\xa0");
+    if (quantity)
+        document.querySelector('.botonenviar span').textContent = quantity;
+    let results = localStorage.getItem("resultsNFL");
+    if (results){
+        results = results.split("*");
+        for (var i = 0; i < quantity; i++){
+            if (results[i] && results[i] != undefined){
+                display.innerHTML += `<div>`+ results[i] + `</div>`;} 
+            else
+                break;
+        }
+    document.getElementById("total").innerHTML = "Total: $" + quantity*50 +"\n";
+    if (display.childElementCount > 0)
+    display.lastElementChild.innerHTML += `<button id="undo" onclick="remove()"><ion-icon name="arrow-undo-outline"></ion-icon></button>`;
+}  
+}
+
+function selection(element){
+    let index = parseInt(element.id.slice(1)) - 1;
+    let container = document.getElementById("text");
+    if (!element.style.backgroundColor){
+        if (!combinations){
+            validation(element);
+            res[index] = element.id.slice(0,1);
+            }
+        else{
+            res[index] += element.id.slice(0,1);
+            res[index] = res[index].split('_').join('');
+        }
+        element.style.backgroundColor = "rgb(250,30,30)";
+    }
+    else{
+        if(combinations){
+        element.style.backgroundColor = "";
+        console.log(res[index].length);
+        if (res[index] != "_" && res[index].length>1)
+            res[index] = res[index].split(element.id.slice(0,1)).join('');
+        else    
+            res[index] = "_";
+        }
+    }
+    costoactual();
+    container.innerHTML = res.join("\xa0\xa0");
+}
+
+function validation(element){
+    let index = parseInt(element.id.slice(1));
+    document.getElementById("L"+index).style.backgroundColor = null;
+    document.getElementById("V"+index).style.backgroundColor = null;
+}
+
+function number(){
+    quantity = localStorage.getItem("quantityNFL");
+    if (quantity)
+        localStorage.setItem('quantityNFL', ++quantity);
+    else{
+        localStorage.setItem('quantityNFL', 1);
+        quantity = localStorage.getItem("quantityNFL");}
+    document.querySelector('.botonenviar span').textContent = quantity;
+    localStorage.setItem('aliasNFL', name);
+}
+
+function result(){
+    results = localStorage.getItem("resultsNFL");
+    name = document.getElementById("nombre").value;
+    name  = name.split('*').join('');
+    if (results){
+        if (aux > 1)
+        localStorage.setItem('resultsNFL', results + "\n" + res.join("\xa0\xa0") + "\xa0\xa0" + name + " (" + aux + ")" + "*");
+        else
+            localStorage.setItem('resultsNFL', results + "\n" + res.join("\xa0\xa0") + "\xa0\xa0" + name + "*");
+        }
+        else
+        {
+            if (aux > 1)
+            localStorage.setItem('resultsNFL',res.join("\xa0\xa0") + "\xa0\xa0" + name + " (" + aux + ")" +  "*");
+            else    
+                localStorage.setItem('resultsNFL',res.join("\xa0\xa0") + "\xa0\xa0" + name+ "*");}       
+}
+
+function send(){
+    if (!quantity || quantity < 1)
+        save();
+    if (quantity > 0){
+    let whatsapptext = res.join("%20%20")
+    whatsapptext = encodeURI(localStorage.getItem("resultsNFL"));
+    whatsapptext = whatsapptext.split('*').join('%0D');
+    window.location.href = "https://wa.me/523317816346?text="+whatsapptext;}
+}
+
+function save(){
+    name = document.getElementById("nombre").value;
+    if (res.join("\xa0\xa0").includes("_"))
+        alert("Debes llenar todas las casillas");
+    else if (!name){
+        alert("Debes elegir un nombre");
+        document.getElementById("nombre").focus();
+        return 0;}
+    else{
+        if (combinations)
+            calculate();
+        else
+            number();
+
+        result();
+        updatedisplay();
+        clean();}
+}
+
+function deleteall(){
+    if(confirm("Se borrará todo"))
+        {localStorage.clear();
+        location.reload();}
+}
+
+function clean(){
+    res = ['_','_','_','_','_','_','_','_','_','_','_','_','_','_'];
+    let container = document.getElementById("text");
+    container.innerHTML = res.join("\xa0\xa0");
+    spans = document.querySelectorAll(".quiniela span");
+    for (var i=0; i<28;i++)
+        spans[i].style.backgroundColor = "";
+    document.getElementById("costo").innerHTML = "Costo: $0";
+    document.getElementById("numquinielas").innerHTML = "0 Quiniela(s)";
+}
+
+function updatedisplay(){
+    if (aux == undefined)
+        aux=0;
+
+    let display = document.getElementById("display");
+    if (display.childElementCount > 0)
+        display.lastElementChild.removeChild(display.lastElementChild.lastElementChild);
+
+    if (aux > 1)
+        display.innerHTML += `<div>` +  res.join("\xa0\xa0") + "\xa0\xa0" + name + " (" + aux + ")" + "*\xa0\xa0" + `</div>`;
+    else
+        display.innerHTML += `<div>` +  res.join("\xa0\xa0") + "\xa0\xa0" + name + "*\xa0\xa0" + `</div>`;
+
+    document.getElementById("total").innerHTML = "Total: $" + quantity*50;
+
+    if (display.childElementCount > 0)
+    display.lastElementChild.innerHTML += `<button id="undo" onclick="remove()"><ion-icon name="arrow-undo-outline"></ion-icon></button>`;
+    aux = 1;
+}
+
+function recovername(){
+    name = localStorage.getItem("aliasNFL");
+    if (name !=  null && name !="null")
+        document.getElementById("nombre").value = name;
+}
+
+function clearname(){
+    document.getElementById("nombre").value = "";
+}
+
+function allowcombination(){
+    if (!combinations) 
+        combinations = confirm("¿Desea registrar quinielas multiples?");
+    else
+        combinations= false;
+    document.getElementById("checkcombinaciones").checked = combinations;
+    clean();
+}
+
+function calculate(){
+    aux = 1;
+    for (var i=0;i<14;i++){
+        aux*= res[i].length;
+    }
+    quantity = localStorage.getItem("quantityNFL");
+    if (quantity){
+        localStorage.setItem('quantityNFL', parseInt(quantity)+aux);
+        quantity = localStorage.getItem('quantityNFL');}
+    else{
+        localStorage.setItem('quantityNFL', aux);
+        quantity = localStorage.getItem("quantityNFL");}
+    document.querySelector('.botonenviar span').textContent = quantity;
+    localStorage.setItem('aliasNFL', name);
+}
+
+function random(){
+    clean();
+    let container = document.getElementById("text");
+    let partidos = document.getElementsByClassName("partido");
+    for (var i = 0; i < 14; i++){
+        var r = getRandomInt(0,1);
+        partidos[i].getElementsByTagName("span")[r].style.backgroundColor = "rgb(250,30,30)";
+        res[i] = ["L","V"][r];
+    }
+    container.innerHTML = res.join("\xa0\xa0");
+    costoactual();
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function costoactual(){
+    if (!res.join("\xa0\xa0").includes("_")){
+        let aux2 = 1;
+        for (var i=0;i<14;i++){
+             aux2*= res[i].length;
+        }
+        document.getElementById("costo").innerHTML = "Costo: $" + aux2*50;
+        document.getElementById("numquinielas").innerHTML = aux2 + " Quiniela(s)"
+    }
+}
+function remove(){
+    if (quantity > 0){
+        x = localStorage.getItem("resultsNFL");
+        x = removeLastLine(x);
+        localStorage.setItem("resultsNFL", x);
+        results = x.split("*");
+
+        let display = document.getElementById('display');
+        last = display.lastElementChild.textContent;
+        last = last.split("\xa0\xa0");
+        last [0] = last[0].split('\n').join('');
+
+        let aux3 = 1;
+        for (var i=0;i<14;i++){
+            aux3*= last[i].length;
+            }
+        quantity -= aux3;
+
+        if (display.childElementCount < 2)
+            localStorage.setItem("resultsNFL","");
+        
+        display.removeChild(display.lastElementChild);
+
+        document.querySelector('.botonenviar span').textContent = quantity;
+        document.getElementById("total").innerHTML = "Total: $" + quantity*50 +"\n";
+
+        localStorage.setItem('quantityNFL', quantity);
+
+        if (display.lastElementChild != null && display.childElementCount > 0)
+            display.lastElementChild.innerHTML += `<button id="undo" onclick="remove()"><ion-icon name="arrow-undo-outline"></ion-icon></button>`;
+        
+}
+}
+
+function removeLastLine(){
+    if(x.lastIndexOf("\n")>0) {
+        return x.substring(0, x.lastIndexOf("\n"));
+    } else {
+        return x;
+    }
+}
+
+window.addEventListener("load",start,false);
